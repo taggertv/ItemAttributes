79a712fa903d19fe8dc78a1e58e0d210aa1274ac
Richard Harrah <topplethenunnery@gmail.com>
2013-10-22 21:25:52 -0400

    adding getMeleeDamage and getRangedDamage

--- src/main/java/net/nunnerycode/bukkit/itemstats/ParseUtil.java
+++ src/main/java/net/nunnerycode/bukkit/itemstats/ParseUtil.java
@@ -45,6 +45,42 @@ public final class ParseUtil {
 		return d;
 	}
 
+	public static double getMeleeDamage(Collection<String> collection, String format) {
+		double d = 0.0;
+		if (collection == null || collection.isEmpty() || format == null || format.isEmpty()) {
+			return d;
+		}
+		for (String s : collection) {
+			String stripped = ChatColor.stripColor(s);
+			String withoutNumbers = stripped.replaceAll("[0-9\\+%\\-]", "").trim();
+			String withoutLetters = stripped.replaceAll("[a-zA-Z\\+%:]", "").trim();
+			String withoutVariables = format.replaceAll("%(?s)(.*?)%", "").trim();
+			if (!withoutNumbers.equals(withoutVariables)) {
+				continue;
+			}
+			d += NumberUtils.toDouble(withoutLetters, 0.0);
+		}
+		return d;
+	}
+
+	public static double getRangedDamage(Collection<String> collection, String format) {
+		double d = 0.0;
+		if (collection == null || collection.isEmpty() || format == null || format.isEmpty()) {
+			return d;
+		}
+		for (String s : collection) {
+			String stripped = ChatColor.stripColor(s);
+			String withoutNumbers = stripped.replaceAll("[0-9\\+%\\-]", "").trim();
+			String withoutLetters = stripped.replaceAll("[a-zA-Z\\+%:]", "").trim();
+			String withoutVariables = format.replaceAll("%(?s)(.*?)%", "").trim();
+			if (!withoutNumbers.equals(withoutVariables)) {
+				continue;
+			}
+			d += NumberUtils.toDouble(withoutLetters, 0.0);
+		}
+		return d;
+	}
+
 	public static double getCriticalDamage(Collection<String> collection, String format) {
 		double d = 0.0;
 		if (collection == null || collection.isEmpty()) {
