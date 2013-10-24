98c98ec92f61bfd2d248a5c872455bc57567c6a5
Richard Harrah <topplethenunnery@gmail.com>
2013-10-24 09:46:17 -0400

    adding a method to get the armor penetration

--- src/main/java/net/nunnerycode/bukkit/itemstats/ParseUtil.java
+++ src/main/java/net/nunnerycode/bukkit/itemstats/ParseUtil.java
@@ -99,6 +99,24 @@ public final class ParseUtil {
 		return d;
 	}
 
+	public static double getArmorPenetration(Collection<String> collection, String format) {
+		double d = 0.0;
+		if (collection == null || collection.isEmpty()) {
+			return d;
+		}
+		for (String s : collection) {
+			String stripped = ChatColor.stripColor(s);
+			String withoutNumbers = stripped.replaceAll("[0-9\\+%\\-]", "").trim();
+			String withoutLetters = stripped.replaceAll("[a-zA-Z\\+%:]", "").trim();
+			String withoutVariables = format.replaceAll("%(?s)(.*?)%", "").trim();
+			if (!withoutNumbers.equals(withoutVariables)) {
+				continue;
+			}
+			d += NumberUtils.toDouble(withoutLetters, 0.0) / 100D;
+		}
+		return d;
+	}
+
 	public static boolean hasPoisonImmunity(Collection<String> collection, String format) {
 		boolean b = false;
 		if (collection == null || collection.isEmpty() || format == null || format.isEmpty()) {
