9d720d3c15676dd94cf6141d76950dd6bef84e03
Richard Harrah <topplethenunnery@gmail.com>
2013-10-24 09:56:52 -0400

    removing old methods and switching to more general methods

--- src/main/java/net/nunnerycode/bukkit/itemstats/ParseUtil.java
+++ src/main/java/net/nunnerycode/bukkit/itemstats/ParseUtil.java
@@ -9,7 +9,7 @@ public final class ParseUtil {
 	private ParseUtil() {
 	}
 
-	public static double getHealth(Collection<String> collection, String format) {
+	public static double getDouble(Collection<String> collection, String format) {
 		double d = 0.0;
 		if (collection == null || collection.isEmpty() || format == null || format.isEmpty()) {
 			return d;
@@ -27,61 +27,7 @@ public final class ParseUtil {
 		return d;
 	}
 
-	public static double getDamage(Collection<String> collection, String format) {
-		double d = 0.0;
-		if (collection == null || collection.isEmpty() || format == null || format.isEmpty()) {
-			return d;
-		}
-		for (String s : collection) {
-			String stripped = ChatColor.stripColor(s);
-			String withoutNumbers = stripped.replaceAll("[0-9\\+%\\-]", "").trim();
-			String withoutLetters = stripped.replaceAll("[a-zA-Z\\+%:]", "").trim();
-			String withoutVariables = format.replaceAll("%(?s)(.*?)%", "").trim();
-			if (!withoutNumbers.equals(withoutVariables)) {
-				continue;
-			}
-			d += NumberUtils.toDouble(withoutLetters, 0.0);
-		}
-		return d;
-	}
-
-	public static double getMeleeDamage(Collection<String> collection, String format) {
-		double d = 0.0;
-		if (collection == null || collection.isEmpty() || format == null || format.isEmpty()) {
-			return d;
-		}
-		for (String s : collection) {
-			String stripped = ChatColor.stripColor(s);
-			String withoutNumbers = stripped.replaceAll("[0-9\\+%\\-]", "").trim();
-			String withoutLetters = stripped.replaceAll("[a-zA-Z\\+%:]", "").trim();
-			String withoutVariables = format.replaceAll("%(?s)(.*?)%", "").trim();
-			if (!withoutNumbers.equals(withoutVariables)) {
-				continue;
-			}
-			d += NumberUtils.toDouble(withoutLetters, 0.0);
-		}
-		return d;
-	}
-
-	public static double getRangedDamage(Collection<String> collection, String format) {
-		double d = 0.0;
-		if (collection == null || collection.isEmpty() || format == null || format.isEmpty()) {
-			return d;
-		}
-		for (String s : collection) {
-			String stripped = ChatColor.stripColor(s);
-			String withoutNumbers = stripped.replaceAll("[0-9\\+%\\-]", "").trim();
-			String withoutLetters = stripped.replaceAll("[a-zA-Z\\+%:]", "").trim();
-			String withoutVariables = format.replaceAll("%(?s)(.*?)%", "").trim();
-			if (!withoutNumbers.equals(withoutVariables)) {
-				continue;
-			}
-			d += NumberUtils.toDouble(withoutLetters, 0.0);
-		}
-		return d;
-	}
-
-	public static double getCriticalDamage(Collection<String> collection, String format) {
+	public static double getDoublePercentage(Collection<String> collection, String format) {
 		double d = 0.0;
 		if (collection == null || collection.isEmpty()) {
 			return d;
@@ -99,115 +45,7 @@ public final class ParseUtil {
 		return d;
 	}
 
-	public static double getArmorPenetration(Collection<String> collection, String format) {
-		double d = 0.0;
-		if (collection == null || collection.isEmpty()) {
-			return d;
-		}
-		for (String s : collection) {
-			String stripped = ChatColor.stripColor(s);
-			String withoutNumbers = stripped.replaceAll("[0-9\\+%\\-]", "").trim();
-			String withoutLetters = stripped.replaceAll("[a-zA-Z\\+%:]", "").trim();
-			String withoutVariables = format.replaceAll("%(?s)(.*?)%", "").trim();
-			if (!withoutNumbers.equals(withoutVariables)) {
-				continue;
-			}
-			d += NumberUtils.toDouble(withoutLetters, 0.0) / 100D;
-		}
-		return d;
-	}
-
-	public static boolean hasPoisonImmunity(Collection<String> collection, String format) {
-		boolean b = false;
-		if (collection == null || collection.isEmpty() || format == null || format.isEmpty()) {
-			return b;
-		}
-		b = containsIgnoreCase(collection, format);
-		return b;
-	}
-
-	public static boolean hasFireImmunity(Collection<String> collection, String format) {
-		boolean b = false;
-		if (collection == null || collection.isEmpty() || format == null || format.isEmpty()) {
-			return b;
-		}
-		b = containsIgnoreCase(collection, format);
-		return b;
-	}
-
-	public static boolean hasWitherImmunity(Collection<String> collection, String format) {
-		boolean b = false;
-		if (collection == null || collection.isEmpty() || format == null || format.isEmpty()) {
-			return b;
-		}
-		b = containsIgnoreCase(collection, format);
-		return b;
-	}
-
-	private static boolean containsIgnoreCase(Collection<String> collection, String string) {
-		for (String s : collection) {
-			if (ChatColor.stripColor(s).equalsIgnoreCase(ChatColor.stripColor(string))) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public static double getRegeneration(Collection<String> collection, String format) {
-		double d = 0.0;
-		if (collection == null || collection.isEmpty() || format == null || format.isEmpty()) {
-			return d;
-		}
-		for (String s : collection) {
-			String stripped = ChatColor.stripColor(s);
-			String withoutNumbers = stripped.replaceAll("[0-9\\+%\\-]", "").trim();
-			String withoutLetters = stripped.replaceAll("[a-zA-Z\\+%:]", "").trim();
-			String withoutVariables = format.replaceAll("%(?s)(.*?)%", "").trim();
-			if (!withoutNumbers.equals(withoutVariables)) {
-				continue;
-			}
-			d += NumberUtils.toDouble(withoutLetters, 0.0);
-		}
-		return d;
-	}
-
-	public static double getArmor(Collection<String> collection, String format) {
-		double d = 0.0;
-		if (collection == null || collection.isEmpty() || format == null || format.isEmpty()) {
-			return d;
-		}
-		for (String s : collection) {
-			String stripped = ChatColor.stripColor(s);
-			String withoutNumbers = stripped.replaceAll("[0-9\\+%\\-]", "").trim();
-			String withoutLetters = stripped.replaceAll("[a-zA-Z\\+%:]", "").trim();
-			String withoutVariables = format.replaceAll("%(?s)(.*?)%", "").trim();
-			if (!withoutNumbers.equals(withoutVariables)) {
-				continue;
-			}
-			d += NumberUtils.toDouble(withoutLetters, 0.0);
-		}
-		return d;
-	}
-
-	public static double getCriticalRate(Collection<String> collection, String format) {
-		double d = 0.0;
-		if (collection == null || collection.isEmpty() || format == null || format.isEmpty()) {
-			return d;
-		}
-		for (String s : collection) {
-			String stripped = ChatColor.stripColor(s);
-			String withoutNumbers = stripped.replaceAll("[0-9\\+%\\-]", "").trim();
-			String withoutLetters = stripped.replaceAll("[a-zA-Z\\+%:]", "").trim();
-			String withoutVariables = format.replaceAll("%(?s)(.*?)%", "").trim();
-			if (!withoutNumbers.equals(withoutVariables)) {
-				continue;
-			}
-			d += NumberUtils.toDouble(withoutLetters, 0.0) / 100D;
-		}
-		return d;
-	}
-
-	public static int getLevelRequired(Collection<String> collection, String format) {
+	public static int getInt(Collection<String> collection, String format) {
 		int i = 0;
 		if (collection == null || collection.isEmpty() || format == null || format.isEmpty()) {
 			return i;
@@ -223,6 +61,24 @@ public final class ParseUtil {
 			i += NumberUtils.toInt(withoutLetters, 0);
 		}
 		return i;
+	}
+
+	public static boolean hasFormatInCollection(Collection<String> collection, String format) {
+		boolean b = false;
+		if (collection == null || collection.isEmpty() || format == null || format.isEmpty()) {
+			return b;
+		}
+		b = containsIgnoreCase(collection, format);
+		return b;
+	}
+
+	private static boolean containsIgnoreCase(Collection<String> collection, String string) {
+		for (String s : collection) {
+			if (ChatColor.stripColor(s).equalsIgnoreCase(ChatColor.stripColor(string))) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
