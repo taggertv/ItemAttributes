c3142490c13c124e4213dc222d9c3098654383a8
Richard Harrah <topplethenunnery@gmail.com>
2013-11-03 14:24:53 -0500

    updating task to correctly update health

--- src/main/java/net/nunnerycode/bukkit/itemattributes/tasks/ItemAttributesHealthUpdateTask.java
+++ src/main/java/net/nunnerycode/bukkit/itemattributes/tasks/ItemAttributesHealthUpdateTask.java
@@ -2,10 +2,11 @@ package net.nunnerycode.bukkit.itemattributes.tasks;
 
 import java.util.ArrayList;
 import java.util.List;
-import net.nunnerycode.bukkit.itemattributes.utils.ItemAttributesParseUtil;
 import net.nunnerycode.bukkit.itemattributes.ItemAttributesPlugin;
 import net.nunnerycode.bukkit.itemattributes.api.ItemAttributes;
 import net.nunnerycode.bukkit.itemattributes.api.tasks.HealthUpdateTask;
+import net.nunnerycode.bukkit.itemattributes.events.ItemAttributesHealthUpdateEvent;
+import net.nunnerycode.bukkit.itemattributes.utils.ItemAttributesParseUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
@@ -41,7 +42,17 @@ public final class ItemAttributesHealthUpdateTask extends BukkitRunnable impleme
 							getPlugin().getSettingsManager().getHealthFormat());
 					double currentHealth = player.getHealth();
 					double baseMaxHealth = getPlugin().getSettingsManager().getBasePlayerHealth();
-					player.setMaxHealth(Math.max(baseMaxHealth + d, 1));
+
+					ItemAttributesHealthUpdateEvent healthUpdateEvent = new ItemAttributesHealthUpdateEvent(player,
+							player.getMaxHealth(), baseMaxHealth, d);
+					Bukkit.getPluginManager().callEvent(healthUpdateEvent);
+
+					if (healthUpdateEvent.isCancelled()) {
+						return;
+					}
+
+					player.setMaxHealth(Math.max(healthUpdateEvent.getBaseHealth() + healthUpdateEvent
+							.getChangeInHealth(), 1));
 					player.setHealth(Math.min(Math.max(currentHealth, 0), player.getMaxHealth()));
 					player.setHealthScale(player.getMaxHealth());
 				} else if (e instanceof LivingEntity) {
@@ -66,7 +77,17 @@ public final class ItemAttributesHealthUpdateTask extends BukkitRunnable impleme
 							}
 						}
 					}
-					entity.setMaxHealth(Math.max(baseMaxHealth + d, 1));
+
+					ItemAttributesHealthUpdateEvent healthUpdateEvent = new ItemAttributesHealthUpdateEvent(entity,
+							entity.getMaxHealth(), baseMaxHealth, d);
+					Bukkit.getPluginManager().callEvent(healthUpdateEvent);
+
+					if (healthUpdateEvent.isCancelled()) {
+						return;
+					}
+
+					entity.setMaxHealth(Math.max(healthUpdateEvent.getBaseHealth() + healthUpdateEvent
+							.getChangeInHealth(), 1));
 					entity.setHealth(Math.min(Math.max(currentHealth, 0), entity.getMaxHealth()));
 				}
 			}
