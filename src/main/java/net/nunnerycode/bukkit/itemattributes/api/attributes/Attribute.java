2b0efca2dc510143e3cc55829270af51e97f5b2e
Richard Harrah <topplethenunnery@gmail.com>
2013-11-14 15:21:08 -0500

    populated interface

--- src/main/java/net/nunnerycode/bukkit/itemattributes/api/attributes/Attribute.java
+++ src/main/java/net/nunnerycode/bukkit/itemattributes/api/attributes/Attribute.java
@@ -1,4 +1,23 @@
 package net.nunnerycode.bukkit.itemattributes.api.attributes;
 
 public interface Attribute {
+
+	boolean isEnabled();
+
+	void setEnabled(boolean b);
+
+	double getMaxValue();
+
+	void setMaxValue(double d);
+
+	boolean isPercentage();
+
+	void setPercentage(boolean b);
+
+	String getFormat();
+
+	void setFormat(String s);
+
+	String getName();
+
 }
