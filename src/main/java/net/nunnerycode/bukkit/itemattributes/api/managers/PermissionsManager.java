61a44aebf861e94e68cfb6894c66ed2966a257c5
Richard Harrah <topplethenunnery@gmail.com>
2013-11-17 22:15:15 -0500

    adding a new interface method to PermissionsManager

--- src/main/java/net/nunnerycode/bukkit/itemattributes/api/managers/PermissionsManager.java
+++ src/main/java/net/nunnerycode/bukkit/itemattributes/api/managers/PermissionsManager.java
@@ -2,6 +2,7 @@ package net.nunnerycode.bukkit.itemattributes.api.managers;
 
 import java.util.List;
 import net.nunnerycode.bukkit.itemattributes.api.ItemAttributes;
+import org.bukkit.entity.Player;
 
 public interface PermissionsManager {
 
@@ -12,5 +13,7 @@ public interface PermissionsManager {
 	void addPermissions(String... permissions);
 
 	void removePermissions(String... permissions);
+
+	boolean hasPermission(Player player, String permission);
 
 }
