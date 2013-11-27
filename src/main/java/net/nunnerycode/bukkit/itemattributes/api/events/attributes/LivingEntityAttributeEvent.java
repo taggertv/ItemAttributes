80ecce96aa75f53e3caf1b4083efe2b014c8afa0
Richard Harrah <topplethenunnery@gmail.com>
2013-11-26 19:46:10 -0500

    adding method to get Attribute-AttributeValue map

--- src/main/java/net/nunnerycode/bukkit/itemattributes/api/events/attributes/LivingEntityAttributeEvent.java
+++ src/main/java/net/nunnerycode/bukkit/itemattributes/api/events/attributes/LivingEntityAttributeEvent.java
@@ -1,9 +1,16 @@
 package net.nunnerycode.bukkit.itemattributes.api.events.attributes;
 
+import java.util.Map;
+import net.nunnerycode.bukkit.itemattributes.api.attributes.Attribute;
+import net.nunnerycode.bukkit.itemattributes.api.attributes.AttributeValue;
 import org.bukkit.entity.LivingEntity;
 
 public interface LivingEntityAttributeEvent {
 
 	LivingEntity getLivingEntity();
+
+	LivingEntity getTarget();
+
+	Map<Attribute, AttributeValue> getAttributeValueMap();
 
 }
