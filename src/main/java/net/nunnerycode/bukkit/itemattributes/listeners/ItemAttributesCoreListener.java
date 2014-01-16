36fa3acfa6e951f23fdeae840c862bcb621f6aca
Richard Harrah <topplethenunnery@gmail.com>
2014-01-16 09:37:03 -0500

    fix for attack speed 2

--- src/main/java/net/nunnerycode/bukkit/itemattributes/listeners/ItemAttributesCoreListener.java
+++ src/main/java/net/nunnerycode/bukkit/itemattributes/listeners/ItemAttributesCoreListener.java
@@ -1089,9 +1089,6 @@ public final class ItemAttributesCoreListener implements Listener, CoreListener 
 						attackSpeedAttribute);
 			}
 
-			Bukkit.getLogger().info("Base attack speed: " + attackSpeedAttribute.getPlayersBaseValue() + " | Attack " +
-					"speed: " + attackSpeed);
-
 			if (((Player) event.getDamager()).hasPermission("itemattributes.testing.spam")) {
 				((Player) event.getDamager()).sendMessage("Attack speed: " + attackSpeed);
 			}
