227d169498edf058cdf548e7ac6fab85e1e74edd
Richard Harrah <rmh4209@gmail.com>
2014-02-02 17:55:57 -0500

    adding javadoc

--- src/main/java/net/nunnerycode/bukkit/itemattributes/api/attributes/IAttribute.java
+++ src/main/java/net/nunnerycode/bukkit/itemattributes/api/attributes/IAttribute.java
@@ -2,10 +2,26 @@ package net.nunnerycode.bukkit.itemattributes.api.attributes;
 
 public interface IAttribute {
 
+  /**
+   * Gets and returns the name of the IAttribute.
+   * @return name of the IAttribute
+   */
   String getName();
 
-  Object getFlagValue(AttributeFlag flag);
+  /**
+   * Gets the value of the AttributeFlag for the IAttribute. Returns the defaultValue if the
+   * AttributeFlag is not set.
+   * @param flag AttributeFlag to check against
+   * @param defaultValue default value of the AttributeFlag
+   * @return value of flag or defaultValue
+   */
+  Object getFlagValue(AttributeFlag flag, Object defaultValue);
 
+  /**
+   * Sets the value of the AttributeFlag.
+   * @param flag AttributeFlag to set for
+   * @param value value to set as
+   */
   void setFlagValue(AttributeFlag flag, Object value);
 
 }
