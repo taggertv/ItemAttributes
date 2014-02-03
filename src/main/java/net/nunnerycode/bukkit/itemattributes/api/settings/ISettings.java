3ec13e483ffb2746a3eb21ad695edb8b295202a3
Richard Harrah <rmh4209@gmail.com>
2014-02-02 23:44:26 -0500

    adding javadoc to ISettings

--- src/main/java/net/nunnerycode/bukkit/itemattributes/api/settings/ISettings.java
+++ src/main/java/net/nunnerycode/bukkit/itemattributes/api/settings/ISettings.java
@@ -2,8 +2,19 @@ package net.nunnerycode.bukkit.itemattributes.api.settings;
 
 public interface ISettings {
 
+  /**
+   * Gets and returns a value for a key, returning defaultValue if key doesn't have a value.
+   * @param key key to check for
+   * @param defaultValue default value to return if no value found
+   * @return value for key
+   */
   Object getSettingValue(String key, Object defaultValue);
 
+  /**
+   * Sets the value for a key
+   * @param key key to set for
+   * @param value value to set for the key
+   */
   void setSettingValue(String key, Object value);
 
 }
