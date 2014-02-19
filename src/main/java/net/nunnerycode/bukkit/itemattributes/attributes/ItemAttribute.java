170399afb72ef5f7b02659e3ac9460e40cd845c3
Richard Harrah <rmh4209@gmail.com>
2014-02-19 18:57:08 -0500

    rewritten ItemAttribute

--- src/main/java/net/nunnerycode/bukkit/itemattributes/attributes/ItemAttribute.java
+++ src/main/java/net/nunnerycode/bukkit/itemattributes/attributes/ItemAttribute.java
@@ -1,63 +1,30 @@
 package net.nunnerycode.bukkit.itemattributes.attributes;
 
-import net.nunnerycode.bukkit.itemattributes.api.attributes.AttributeFlag;
 import net.nunnerycode.bukkit.itemattributes.api.attributes.IAttribute;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import org.bukkit.Effect;
+import org.bukkit.Sound;
 
 public final class ItemAttribute implements IAttribute {
 
   private String name;
-  private Map<AttributeFlag, Object> flagValueMap;
+  private boolean enabled;
+  private Sound sound;
+  private Effect effect;
+  private boolean percentage;
+  private double percentageDivisor;
+  private double baseValue;
+  private double minimumValue;
+  private double maximumValue;
+  private String format;
 
   public ItemAttribute(String name) {
     this.name = name != null ? name : "";
-    flagValueMap = new ConcurrentHashMap<>();
   }
 
-  /**
-   * Gets and returns the name of the IAttribute.
-   *
-   * @return name of the IAttribute
-   */
   @Override
   public String getName() {
     return name;
-  }
-
-  /**
-   * Gets the value of the AttributeFlag for the IAttribute. Returns the defaultValue if the
-   * AttributeFlag is not set.
-   *
-   * @param flag         AttributeFlag to check against
-   * @param defaultValue default value of the AttributeFlag
-   * @return value of flag or defaultValue
-   */
-  @Override
-  public Object getFlagValue(AttributeFlag flag, Object defaultValue) {
-    return flagValueMap.containsKey(flag) ? flagValueMap.get(flag) : defaultValue;
-  }
-
-  /**
-   * Sets the value of the AttributeFlag. </br> Throws an IllegalArgumentException if the value does
-   * not pass match the flag's accepted type.
-   *
-   * @param flag  AttributeFlag to set for
-   * @param value value to set as
-   * @throws java.lang.IllegalArgumentException when Object does not match type
-   */
-  @Override
-  public void setFlagValue(AttributeFlag flag, Object value) throws IllegalArgumentException {
-    if (value == null && flagValueMap.containsKey(flag)) {
-      flagValueMap.remove(flag);
-      return;
-    }
-    if (!flag.matchesType(value)) {
-      throw new IllegalArgumentException("value does not match AttributeFlag's type: " + flag
-          .getMatchingType().getName());
-    }
-    flagValueMap.put(flag, value);
   }
 
   @Override
@@ -79,4 +46,93 @@ public final class ItemAttribute implements IAttribute {
     return name.hashCode();
   }
 
+  @Override
+  public boolean isEnabled() {
+    return enabled;
+  }
+
+  @Override
+  public void setEnabled(boolean enabled) {
+    this.enabled = enabled;
+  }
+
+  @Override
+  public Sound getSound() {
+    return sound;
+  }
+
+  @Override
+  public void setSound(Sound sound) {
+    this.sound = sound;
+  }
+
+  @Override
+  public Effect getEffect() {
+    return effect;
+  }
+
+  @Override
+  public void setEffect(Effect effect) {
+    this.effect = effect;
+  }
+
+  @Override
+  public boolean isPercentage() {
+    return percentage;
+  }
+
+  @Override
+  public void setPercentage(boolean percentage) {
+    this.percentage = percentage;
+  }
+
+  @Override
+  public double getPercentageDivisor() {
+    return percentageDivisor;
+  }
+
+  @Override
+  public void setPercentageDivisor(double percentageDivisor) {
+    this.percentageDivisor = percentageDivisor;
+  }
+
+  @Override
+  public double getBaseValue() {
+    return baseValue;
+  }
+
+  @Override
+  public void setBaseValue(double baseValue) {
+    this.baseValue = baseValue;
+  }
+
+  @Override
+  public double getMinimumValue() {
+    return minimumValue;
+  }
+
+  @Override
+  public void setMinimumValue(double minimumValue) {
+    this.minimumValue = minimumValue;
+  }
+
+  @Override
+  public double getMaximumValue() {
+    return maximumValue;
+  }
+
+  @Override
+  public void setMaximumValue(double maximumValue) {
+    this.maximumValue = maximumValue;
+  }
+
+  @Override
+  public String getFormat() {
+    return format;
+  }
+
+  @Override
+  public void setFormat(String format) {
+    this.format = format;
+  }
 }
