c7b439800e6aa1aa0f125888e43aba261a215676
Richard Harrah <rmh4209@gmail.com>
2014-02-19 19:56:07 -0500

    improving AttributeHandlers and removing guava

--- src/main/java/net/nunnerycode/bukkit/itemattributes/api/attributes/IAttributeHandler.java
+++ src/main/java/net/nunnerycode/bukkit/itemattributes/api/attributes/IAttributeHandler.java
@@ -1,10 +1,12 @@
 package net.nunnerycode.bukkit.itemattributes.api.attributes;
 
+import org.bukkit.Effect;
 import org.bukkit.Location;
+import org.bukkit.Sound;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Set;
+import java.util.List;
 
 public interface IAttributeHandler {
 
@@ -14,51 +16,29 @@ public interface IAttributeHandler {
    * @param attribute IAttribute to check for
    * @return value of IAttribute on ItemStack
    */
-  IAttributeValue getAttributeValueFromItemStack(ItemStack itemStack, IAttribute attribute);
+  List<IAttributeValue> getAttributeValueFromItemStack(ItemStack itemStack, IAttribute attribute);
 
   /**
    * Gets and returns the value of the {@link IAttribute} on the LivingEntity
    * </br>
-   * Equivalent of calling {@link #getAttributesPresentOnItemStack(ItemStack)} on all {@link
-   * ItemStack}s in {@link org.bukkit.entity.LivingEntity#getEquipment()}.
+   * Equivalent of calling {@link #getAttributeValueFromItemStack(ItemStack, IAttribute)} on all {@link
+   * ItemStack}s in {@link LivingEntity#getEquipment()}.
    * @param livingEntity LivingEntity to check on
    * @param attribute IAttribute to check for
    * @return value of IAttribute on ItemStack
    */
-  IAttributeValue getAttributeValueFromEntity(LivingEntity livingEntity, IAttribute attribute);
+  List<IAttributeValue> getAttributeValueFromEntity(LivingEntity livingEntity,
+                                                   IAttribute attribute);
 
   /**
-   * Gets and returns a {@link Set} of {@link IAttribute}s contained on an {@link ItemStack}.
-   * @param itemStack ItemStack to check
-   * @return Set of IAttributes
-   */
-  Set<IAttribute> getAttributesPresentOnItemStack(ItemStack itemStack);
-
-  /**
-   * Returns true if an {@link ItemStack} has an {@link IAttribute} on it.
-   * @param itemStack ItemStack to check
-   * @param attribute IAttribute to check for
-   * @return if ItemStack has the IAttribute
-   */
-  boolean hasAttributeOnItemStack(ItemStack itemStack, IAttribute attribute);
-
-  /**
-   * Returns true if an {@link LivingEntity} has an {@link IAttribute} on it.
-   * @param livingEntity ItemStack to check
-   * @param attribute IAttribute to check for
-   * @return if LivingEntity has the IAttribute
-   */
-  boolean hasAttributeOnEntity(LivingEntity livingEntity, IAttribute attribute);
-
-  /**
-   * Plays the {@link org.bukkit.Effect} of some {@link IAttribute}s at a {@link Location}.
+   * Plays the {@link Effect} of some {@link IAttribute}s at a {@link Location}.
    * @param location Location to play Effects
    * @param attributes IAttributes to use for Sounds
    */
   void playAttributeEffects(Location location, IAttribute... attributes);
 
   /**
-   * Plays the {@link org.bukkit.Sound} of some {@link IAttribute}s at a {@link Location}.
+   * Plays the {@link Sound} of some {@link IAttribute}s at a {@link Location}.
    * @param location Location to play Sounds
    * @param attributes IAttributes to use for Sounds
    */
--- src/main/java/net/nunnerycode/bukkit/itemattributes/attributes/ItemAttributeHandler.java
+++ src/main/java/net/nunnerycode/bukkit/itemattributes/attributes/ItemAttributeHandler.java
@@ -2,25 +2,67 @@ package net.nunnerycode.bukkit.itemattributes.attributes;
 
 import net.nunnerycode.bukkit.itemattributes.api.attributes.IAttribute;
 import net.nunnerycode.bukkit.itemattributes.api.attributes.IAttributeHandler;
+import net.nunnerycode.bukkit.itemattributes.api.attributes.IAttributeValue;
 
+import org.apache.commons.lang.math.RandomUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class ItemAttributeHandler implements IAttributeHandler {
 
   /**
    * Gets and returns the value of the {@link net.nunnerycode.bukkit.itemattributes.api.attributes.IAttribute} on the {@link org.bukkit.inventory.ItemStack}
    *
+   *
    * @param itemStack ItemStack to check
    * @param attribute IAttribute to check for
    * @return value of IAttribute on ItemStack
    */
   @Override
-  public ItemAttributeValue getAttributeValueFromItemStack(ItemStack itemStack, IAttribute attribute) {
-    return new ItemAttributeValue(0);
+  public List<IAttributeValue> getAttributeValueFromItemStack(ItemStack itemStack,
+                                                              IAttribute attribute) {
+    List<IAttributeValue> iAttributeValues = new ArrayList<>();
+    if (itemStack == null || attribute == null) {
+      return iAttributeValues;
+    }
+    if (!itemStack.hasItemMeta()) {
+      return iAttributeValues;
+    }
+    if (!itemStack.getItemMeta().hasLore()) {
+      return iAttributeValues;
+    }
+    String format = attribute.getFormat();
+    String formatWithoutVariables = format.replaceAll("%(?s)(.*?)%", "").trim();
+    for (String s : itemStack.getItemMeta().getLore()) {
+      String stripped = ChatColor.stripColor(s);
+      String withoutNumbers = stripped.replaceAll("[0-9\\+%\\-]", "").trim();
+      String withoutLetters = stripped.replaceAll(withoutNumbers, "").trim();
+
+      if (!withoutNumbers.equals(formatWithoutVariables)) {
+        continue;
+      }
+
+      if (withoutLetters.contains("-")) {
+        String[] split = withoutLetters.split("-");
+        if (split.length > 1) {
+          double first = NumberUtils.toDouble(split[0].trim(), 0.0);
+          double second = NumberUtils.toDouble(split[1].trim(), 0.0);
+          double d = RandomUtils.nextDouble() * (Math.max(first, second) - Math.min(first,
+                                                                              second)) + Math.min(first, second);
+          iAttributeValues.add(new ItemAttributeValue(d));
+        }
+      } else {
+        iAttributeValues.add(new ItemAttributeValue(stripped.replace(formatWithoutVariables,
+                                                                     "").trim()));
+      }
+    }
+    return iAttributeValues;
   }
 
   /**
@@ -28,49 +70,21 @@ public final class ItemAttributeHandler implements IAttributeHandler {
    * calling {@link #getAttributesPresentOnItemStack(org.bukkit.inventory.ItemStack)} on all {@link org.bukkit.inventory.ItemStack}s in {@link
    * org.bukkit.entity.LivingEntity#getEquipment()}.
    *
+   *
    * @param livingEntity LivingEntity to check on
    * @param attribute    IAttribute to check for
    * @return value of IAttribute on ItemStack
    */
   @Override
-  public ItemAttributeValue getAttributeValueFromEntity(LivingEntity livingEntity, IAttribute attribute) {
-    return new ItemAttributeValue(0);
-  }
-
-  /**
-   * Gets and returns a {@link java.util.Set} of {@link net.nunnerycode.bukkit.itemattributes.api.attributes.IAttribute}s
-   * contained on an {@link org.bukkit.inventory.ItemStack}.
-   *
-   * @param itemStack ItemStack to check
-   * @return Set of IAttributes
-   */
-  @Override
-  public Set<IAttribute> getAttributesPresentOnItemStack(ItemStack itemStack) {
-    return null;
-  }
-
-  /**
-   * Returns true if an {@link org.bukkit.inventory.ItemStack} has an {@link net.nunnerycode.bukkit.itemattributes.api.attributes.IAttribute} on it.
-   *
-   * @param itemStack ItemStack to check
-   * @param attribute IAttribute to check for
-   * @return if ItemStack has the IAttribute
-   */
-  @Override
-  public boolean hasAttributeOnItemStack(ItemStack itemStack, IAttribute attribute) {
-    return false;
-  }
-
-  /**
-   * Returns true if an {@link org.bukkit.entity.LivingEntity} has an {@link net.nunnerycode.bukkit.itemattributes.api.attributes.IAttribute} on it.
-   *
-   * @param livingEntity ItemStack to check
-   * @param attribute    IAttribute to check for
-   * @return if LivingEntity has the IAttribute
-   */
-  @Override
-  public boolean hasAttributeOnEntity(LivingEntity livingEntity, IAttribute attribute) {
-    return false;
+  public List<IAttributeValue> getAttributeValueFromEntity(LivingEntity livingEntity,
+                                                           IAttribute attribute) {
+    List<IAttributeValue> values = new ArrayList<>();
+    for (ItemStack is : livingEntity.getEquipment().getArmorContents()) {
+      values.addAll(getAttributeValueFromItemStack(is, attribute));
+    }
+    values.addAll(getAttributeValueFromItemStack(livingEntity.getEquipment().getItemInHand(),
+                                                 attribute));
+    return values;
   }
 
   /**
