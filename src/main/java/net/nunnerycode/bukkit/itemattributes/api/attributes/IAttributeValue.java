67c2877399c8decba71d2a4eab55171d9f94933e
Richard Harrah <rmh4209@gmail.com>
2014-02-19 19:25:14 -0500

    adding IAttributeValue interface

---
+++ src/main/java/net/nunnerycode/bukkit/itemattributes/api/attributes/IAttributeValue.java
@@ -0,0 +1,15 @@
+package net.nunnerycode.bukkit.itemattributes.api.attributes;
+
+public interface IAttributeValue {
+
+  double asDouble();
+
+  int asInt();
+
+  long asLong();
+
+  String asString();
+
+  boolean asBoolean();
+
+}
