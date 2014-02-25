4ffe29c287463eef6f84424665b5cfc7959f13b9
Richard Harrah <topplethenunnery@gmail.com>
2014-02-25 03:01:56 -0500

    removing some methods from IAttribute

--- src/main/java/net/nunnerycode/bukkit/itemattributes/api/attributes/IAttribute.java
+++ src/main/java/net/nunnerycode/bukkit/itemattributes/api/attributes/IAttribute.java
@@ -9,38 +9,20 @@ public interface IAttribute {
 
   boolean isEnabled();
 
-  void setEnabled(boolean b);
-
   boolean isPercentage();
-
-  void setPercentage(boolean b);
 
   String getFormat();
 
-  void setFormat(String s);
-
   double getPercentageDivisor();
-
-  void setPercentageDivisor(double d);
 
   Sound getSound();
 
-  void setSound(Sound s);
-
   Effect getEffect();
-
-  void setEffect(Effect e);
 
   double getBaseValue();
 
-  void setBaseValue(double d);
-
   double getMinimumValue();
 
-  void setMinimumValue(double d);
-
   double getMaximumValue();
-
-  void setMaximumValue(double d);
 
 }
