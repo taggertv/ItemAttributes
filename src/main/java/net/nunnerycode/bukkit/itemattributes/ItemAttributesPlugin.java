d1d10f3cc97b434c0760dfcfdd09b43af82ef9ad
Richard Harrah <topplethenunnery@gmail.com>
2014-02-25 04:07:23 -0500

    loading yaml files

--- src/main/java/net/nunnerycode/bukkit/itemattributes/ItemAttributesPlugin.java
+++ src/main/java/net/nunnerycode/bukkit/itemattributes/ItemAttributesPlugin.java
@@ -6,17 +6,25 @@ import net.nunnerycode.bukkit.itemattributes.api.settings.ISettings;
 import net.nunnerycode.bukkit.itemattributes.attributes.ItemAttributeBuilder;
 import net.nunnerycode.bukkit.itemattributes.attributes.ItemAttributeHandler;
 import net.nunnerycode.bukkit.itemattributes.settings.ItemAttributesSettings;
+import net.nunnerycode.bukkit.libraries.ivory.config.VersionedIvoryYamlConfiguration;
 import net.nunnerycode.java.libraries.cannonball.DebugPrinter;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
 import java.util.logging.Level;
+
+import static net.nunnerycode.bukkit.libraries.ivory.config.VersionedIvoryYamlConfiguration.VersionUpdateType;
 
 public final class ItemAttributesPlugin extends JavaPlugin {
 
   private IAttributeHandler attributeHandler;
   private ISettings settings;
   private DebugPrinter debugPrinter;
+  private VersionedIvoryYamlConfiguration attributesYAML;
+  private VersionedIvoryYamlConfiguration configYAML;
+  private VersionedIvoryYamlConfiguration languageYAML;
+  private VersionedIvoryYamlConfiguration permissionsYAML;
 
   public IAttributeHandler getAttributeHandler() {
     return attributeHandler;
@@ -31,6 +39,42 @@ public final class ItemAttributesPlugin extends JavaPlugin {
     settings = new ItemAttributesSettings();
     attributeHandler = new ItemAttributeHandler();
 
+    attributesYAML =
+        new VersionedIvoryYamlConfiguration(new File(getDataFolder(), "attributes.yml"),
+                                            getResource("attributes.yml"),
+                                            VersionUpdateType.BACKUP_AND_UPDATE);
+    if (attributesYAML.update()) {
+      debug(Level.INFO, "updating attributes.yml");
+    }
+    attributesYAML.load();
+
+    configYAML =
+        new VersionedIvoryYamlConfiguration(new File(getDataFolder(), "config.yml"),
+                                            getResource("config.yml"),
+                                            VersionUpdateType.BACKUP_AND_UPDATE);
+    if (configYAML.update()) {
+      debug(Level.INFO, "updating config.yml");
+    }
+    configYAML.load();
+
+    languageYAML =
+        new VersionedIvoryYamlConfiguration(new File(getDataFolder(), "language.yml"),
+                                            getResource("language.yml"),
+                                            VersionUpdateType.BACKUP_AND_UPDATE);
+    if (languageYAML.update()) {
+      debug(Level.INFO, "updating language.yml");
+    }
+    languageYAML.load();
+
+    permissionsYAML =
+        new VersionedIvoryYamlConfiguration(new File(getDataFolder(), "permissions.yml"),
+                                            getResource("permissions.yml"),
+                                            VersionUpdateType.BACKUP_AND_UPDATE);
+    if (permissionsYAML.update()) {
+      debug(Level.INFO, "updating permissions.yml");
+    }
+    permissionsYAML.load();
+
     debug(Level.INFO, "v" + getDescription().getVersion() + " enabled");
   }
 
@@ -43,6 +87,22 @@ public final class ItemAttributesPlugin extends JavaPlugin {
 
   public IAttributeBuilder getAttributeBuilder(String name) {
     return new ItemAttributeBuilder(name);
+  }
+
+  public VersionedIvoryYamlConfiguration getAttributesYAML() {
+    return attributesYAML;
+  }
+
+  public VersionedIvoryYamlConfiguration getConfigYAML() {
+    return configYAML;
+  }
+
+  public VersionedIvoryYamlConfiguration getLanguageYAML() {
+    return languageYAML;
+  }
+
+  public VersionedIvoryYamlConfiguration getPermissionsYAML() {
+    return permissionsYAML;
   }
 
 }
