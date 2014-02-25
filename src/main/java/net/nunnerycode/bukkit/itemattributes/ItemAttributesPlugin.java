2b249c982b65f9dae16f164c8419c0ce3b9a305c
Richard Harrah <topplethenunnery@gmail.com>
2014-02-25 04:22:49 -0500

    loading attributes

--- src/main/java/net/nunnerycode/bukkit/itemattributes/ItemAttributesPlugin.java
+++ src/main/java/net/nunnerycode/bukkit/itemattributes/ItemAttributesPlugin.java
@@ -1,17 +1,25 @@
 package net.nunnerycode.bukkit.itemattributes;
 
+import net.nunnerycode.bukkit.itemattributes.api.attributes.IAttribute;
 import net.nunnerycode.bukkit.itemattributes.api.attributes.IAttributeBuilder;
 import net.nunnerycode.bukkit.itemattributes.api.attributes.IAttributeHandler;
 import net.nunnerycode.bukkit.itemattributes.api.settings.ISettings;
+import net.nunnerycode.bukkit.itemattributes.attributes.AttributeMap;
 import net.nunnerycode.bukkit.itemattributes.attributes.ItemAttributeBuilder;
 import net.nunnerycode.bukkit.itemattributes.attributes.ItemAttributeHandler;
 import net.nunnerycode.bukkit.itemattributes.settings.ItemAttributesSettings;
 import net.nunnerycode.bukkit.libraries.ivory.config.VersionedIvoryYamlConfiguration;
 import net.nunnerycode.java.libraries.cannonball.DebugPrinter;
 
+import org.bukkit.Effect;
+import org.bukkit.Sound;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 
 import static net.nunnerycode.bukkit.libraries.ivory.config.VersionedIvoryYamlConfiguration.VersionUpdateType;
@@ -75,7 +83,43 @@ public final class ItemAttributesPlugin extends JavaPlugin {
     }
     permissionsYAML.load();
 
+    loadAttributes();
+
     debug(Level.INFO, "v" + getDescription().getVersion() + " enabled");
+  }
+
+  private void loadAttributes() {
+    YamlConfiguration c = attributesYAML;
+    if (!c.isConfigurationSection("attributes")) {
+      return;
+    }
+    ConfigurationSection attributes = c.getConfigurationSection("attributes");
+    List<String> loadedAttributes = new ArrayList<>();
+    AttributeMap.getInstance().clear();
+    for (String key : attributes.getKeys(false)) {
+      if (!c.isConfigurationSection(key)) {
+        continue;
+      }
+      ConfigurationSection cs = c.getConfigurationSection(key);
+      IAttributeBuilder attributeBuilder = getAttributeBuilder(key);
+      attributeBuilder.withFormat(cs.getString("format"));
+      attributeBuilder.withBaseValue(cs.getDouble("base-value", 0));
+      attributeBuilder.withMinimumValue(cs.getDouble("minimum-value", 0));
+      attributeBuilder.withMaximumValue(cs.getDouble("maximum-value", 0));
+      try {
+        attributeBuilder.withEffect(Effect.valueOf(cs.getString("effect")));
+        attributeBuilder.withSound(Sound.valueOf(cs.getString("sound")));
+      } catch (Exception e) {
+        // do nothing
+      }
+      attributeBuilder.withPercentageDivisor(cs.getDouble("percentage-divisor", 100));
+      attributeBuilder.withEnabled(cs.getBoolean("enabled", true));
+      attributeBuilder.withPercentage(cs.getBoolean("percentage", false));
+      IAttribute attribute = attributeBuilder.build();
+      loadedAttributes.add(attribute.getName());
+      AttributeMap.getInstance().put(key, attribute);
+    }
+    debug(Level.INFO, "Loaded attributes: " + loadedAttributes.toString());
   }
 
   public void debug(Level level, String... messages) {
