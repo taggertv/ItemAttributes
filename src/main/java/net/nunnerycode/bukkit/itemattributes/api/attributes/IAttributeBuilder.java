08438729529589a7bd1b0a7ebdc9ed82ed2720b8
Richard Harrah <topplethenunnery@gmail.com>
2014-02-25 02:55:13 -0500

    adding IAttributeBuilder interface

---
+++ src/main/java/net/nunnerycode/bukkit/itemattributes/api/attributes/IAttributeBuilder.java
@@ -0,0 +1,26 @@
+package net.nunnerycode.bukkit.itemattributes.api.attributes;
+
+import org.bukkit.Effect;
+import org.bukkit.Sound;
+
+public interface IAttributeBuilder {
+
+  IAttributeBuilder withEnabled(boolean b);
+
+  IAttributeBuilder withPercentage(boolean b);
+
+  IAttributeBuilder withFormat(String s);
+
+  IAttributeBuilder withPercentageDivisor(double d);
+
+  IAttributeBuilder withSound(Sound s);
+
+  IAttributeBuilder withEffect(Effect e);
+
+  IAttributeBuilder withBaseValue(double d);
+
+  IAttributeBuilder withMinimumValue(double d);
+
+  IAttributeBuilder withMaximumValue(double d);
+
+}
