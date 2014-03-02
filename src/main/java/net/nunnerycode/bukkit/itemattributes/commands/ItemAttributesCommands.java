5bf98be577929c10db56a3e4aac162d37c05912a
Richard Harrah <topplethenunnery@gmail.com>
2014-03-02 01:28:53 -0500

    adding ItemAttributesCommands class

---
+++ src/main/java/net/nunnerycode/bukkit/itemattributes/commands/ItemAttributesCommands.java
@@ -0,0 +1,116 @@
+package net.nunnerycode.bukkit.itemattributes.commands;
+
+import net.nunnerycode.bukkit.itemattributes.ItemAttributesPlugin;
+import net.nunnerycode.bukkit.itemattributes.api.attributes.IAttribute;
+import net.nunnerycode.bukkit.itemattributes.attributes.AttributeMap;
+import net.nunnerycode.bukkit.itemattributes.utils.MessageUtil;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import se.ranzdo.bukkit.methodcommand.Arg;
+import se.ranzdo.bukkit.methodcommand.Command;
+
+public class ItemAttributesCommands {
+
+  private ItemAttributesPlugin plugin;
+
+  public ItemAttributesCommands(ItemAttributesPlugin plugin) {
+    this.plugin = plugin;
+  }
+
+  @Command(identifier = "itemattributes view")
+  public void viewSubcommand(CommandSender commandSender,
+                             @Arg(name = "player") String target) {
+    Player player = Bukkit.getPlayer(target);
+    if (player == null) {
+      MessageUtil.sendColoredMessage(commandSender, (String) plugin.getSettings()
+          .getSettingValue("language.messages.commands.target-is-not-player",
+                           "language.messages.commands.target-is-not-player"));
+      return;
+    }
+    String languageHelpKey =
+        (String) plugin.getSettings().getSettingValue("language.messages.commands.view-stats-help",
+                                                      "language.messages.commands.view-stats-help");
+    MessageUtil.sendColoredMessage(player, languageHelpKey);
+    String languageKey =
+        (String) plugin.getSettings().getSettingValue("language.messages.commands.view-stats",
+                                                      "language.messages.commands.view-stats");
+    String languagePercentageKey =
+        (String) plugin.getSettings().getSettingValue("language.messages.commands.view-stats-percentage",
+                                                      "language.messages.commands.view-stats-percentage");
+    for (IAttribute att : AttributeMap.getInstance().values()) {
+      if (!att.isEnabled()) {
+        continue;
+      }
+      double
+          headValue =
+          plugin.getAttributeHandler()
+              .getAttributeValueFromItemStack(player.getEquipment().getHelmet(), att).asDouble();
+      double
+          chestValue =
+          plugin.getAttributeHandler()
+              .getAttributeValueFromItemStack(player.getEquipment().getChestplate(), att)
+              .asDouble();
+      double
+          legValue =
+          plugin.getAttributeHandler()
+              .getAttributeValueFromItemStack(player.getEquipment().getLeggings(), att).asDouble();
+      double
+          footValue =
+          plugin.getAttributeHandler()
+              .getAttributeValueFromItemStack(player.getEquipment().getBoots(), att).asDouble();
+      double
+          handValue =
+          plugin.getAttributeHandler()
+              .getAttributeValueFromItemStack(player.getEquipment().getItemInHand(), att)
+              .asDouble();
+      double baseValue = att.getBaseValue();
+      double tot = headValue + chestValue + legValue + footValue + handValue;
+      double totalValue;
+      if (att.isPercentage()) {
+        totalValue = Math.min(
+            Math.max(tot,
+                     att.getMinimumValue()), att.getMaximumValue());
+      } else {
+        totalValue = Math.min(
+            Math.max(tot + baseValue,
+                     att.getMinimumValue()), att.getMaximumValue());
+      }
+
+      if (att.isPercentage()) {
+        MessageUtil.sendColoredArgumentMessage(player, languagePercentageKey,
+                                               new String[][]{{"%statname%", att.getName()},
+                                                              {"%totalvalue%",
+                                                               String.valueOf(totalValue)},
+                                                              {"%helmet%",
+                                                               String.valueOf(headValue)},
+                                                              {"%chestplate%",
+                                                               String.valueOf(chestValue)},
+                                                              {"%leggings%",
+                                                               String.valueOf(legValue)},
+                                                              {"%boots%",
+                                                               String.valueOf(footValue)},
+                                                              {"%item%",
+                                                               String.valueOf(handValue)}});
+      } else {
+        MessageUtil.sendColoredArgumentMessage(player, languageKey,
+                                               new String[][]{{"%statname%", att.getName()},
+                                                              {"%totalvalue%",
+                                                               String.valueOf(totalValue)},
+                                                              {"%helmet%",
+                                                               String.valueOf(headValue)},
+                                                              {"%chestplate%",
+                                                               String.valueOf(chestValue)},
+                                                              {"%leggings%",
+                                                               String.valueOf(legValue)},
+                                                              {"%boots%",
+                                                               String.valueOf(footValue)},
+                                                              {"%item%",
+                                                               String.valueOf(handValue)}});
+      }
+    }
+  }
+
+}
